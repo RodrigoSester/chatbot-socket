212d5bb8d63bc8d00c9878fae16c0fb5fb367522
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-06-23 20:25:30 -0300

    refactor: refactor bot's response and client logic

--- src/classes/Client.java
+++ src/classes/Client.java
@@ -4,34 +4,44 @@ import java.net.*;
 import java.io.*;
 
 public class Client {
- 
+	private static final String HOST = "localhost";
+	private static final int PORT = 4567;
+	
     public static void main(String[] args) {
- 
-        String hostname = "localhost";
-        int port = 4567;
- 
-        try (Socket socket = new Socket(hostname, port)) {
-            
+        try (Socket socket = new Socket(HOST, PORT)) {
             System.out.println("Server is connected");
+            System.out.println("Welcome to Brele Chatbot!!");
+            System.out.println("You can do a few questions to me, there are them: ");
+            System.out.println(
+            		"- Que horas sao?\n"
+            		+ "- Vai chover?\n"
+            		+ "- Que horas o sol se poe?\n"
+            		+ "- Qual a temperatura maxima de hoje?\n"
+            		+ "- Qual a temperatura minima de hoje?\n"
+            );
+            
+            System.out.println("And if you want to exit from the chat, type 'Sair'");
+
             OutputStream output = socket.getOutputStream();
             PrintWriter writer = new PrintWriter(output, true);
             
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader systemReader = new BufferedReader(new InputStreamReader(System.in));
             String name;
             
             do {
-            	System.out.println("Enter the Message to the Server:");
-            	name = br.readLine();
-            
-                
+            	System.out.println("--------------------------------");
+            	System.out.println("Enter the Message to Brele:");
+            	name = systemReader.readLine();
+
                 writer.println(name);
  
                 InputStream input = socket.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                BufferedReader inputReader = new BufferedReader(new InputStreamReader(input));
                 
-                String time = reader.readLine();
+                String serverResponse = inputReader.readLine();
  
-                System.out.println(time);
+                System.out.println("Here's the Brele's response:");
+                System.out.println("-> " + serverResponse);
             } while (!name.equals("Sair"));
 
             socket.close();
@@ -39,7 +49,13 @@ public class Client {
         } catch (UnknownHostException ex) {
             System.out.println("Server not found: " + ex.getMessage());
         } catch (IOException ex) {
+        	if (ex.getMessage().equals("Connection reset")) {
+        		System.out.println("The server closed!");
+        		System.out.println("Try to connect again");
+        		return;
+        	}
             System.out.println("I/O error: " + ex.getMessage());
+            return;
         }
     }
 }
--- src/classes/Server.java
+++ src/classes/Server.java
@@ -2,46 +2,83 @@ package classes;
 
 import java.io.*;
 import java.net.*;
+import java.time.LocalDateTime;
+import java.util.Random;
 
 public class Server { 
-    public static void main(String[] args) {
-        int port = 4567;
- 
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Server is listening on port " + port);
+	private static final int PORT = 4567;
+
+    public static void main(String[] args) { 
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server is listening on port " + PORT);
  
             while (true) {
                 try (Socket socket = serverSocket.accept()) {
-                    System.out.println("New client connected");
+                    System.out.println("New client connected!");
                     
                     InputStream input = socket.getInputStream();
                     BufferedReader reader = new BufferedReader(new InputStreamReader(input));
                     
                     OutputStream output = socket.getOutputStream();
                     PrintWriter writer = new PrintWriter(output, true);
-                    
-                    
-                    String text;
+
+                    String clientMessage;
                    
                     do {
-                        text = reader.readLine();
+                        clientMessage = reader.readLine();
 
-                        System.out.println("Client sent Message: "+text);
+                        System.out.println("Client sent Message: " + clientMessage);
 
-                        if(!text.equals("Sair")){
-                           writer.println("Reply from Server: Ok");
-                        }else{
-                        String reply = "Encerrando execução";
-                        writer.println("Reply from Server: " +reply);
+                        if (!clientMessage.equals("Sair")) {                 
+                           String response = getMessageResponse(clientMessage);
+
+                           writer.println(response);
+                        } else {
+	                        writer.println("Closing server...");
                         }
-                        
-                    } while (!text.equals("Sair"));
-                    System.out.println("Connection Closed");
+                    } while (!clientMessage.equals("Sair"));
+
+                    System.out.println("Connection closed on address " + socket.getLocalAddress() + ":" + socket.getLocalPort());
                     socket.close();
+
+                } catch (IOException e) {
+                	if (e.getMessage().equals("Connection reset")) {                		
+                		System.out.println("The client closed the connection!");
+                		continue;
+                	}
+
+                	System.out.println("An error occured: " + e.getMessage());
+                	continue;
                 }
             }
         } catch (IOException ex) {
             System.out.println("Server exception: " + ex.getMessage());
+        }
+    }
+    
+    private static String getMessageResponse(String message) {
+    	Random random = new Random();
+
+    	if (message.equals("Que horas sao?")) {
+    		LocalDateTime now = LocalDateTime.now();
+     	   return ("Hora de agora: " + now.getHour() + ":" + now.getMinute() + ":" + now.getSecond());
+        } else if (message.equals("Que horas o sol se poe?")) {                        	   
+     	   return ("O sol se poe perto das 18h");
+        } else if (message.equals("Vai chover?")) {
+     	    int num = random.nextInt(2);
+   			if (num == 1) {
+   				return ("Sim, chovera hoje!");
+   			} else {                           				
+   				return ("Nao, nao chovera hoje!");
+   			}                        	   
+        } else if (message.equals("Qual a temperatura maxima de hoje?")) {                        	   
+     	   int temperature = random.nextInt(20, 40);
+     	   return ("Temperatura maxima de hoje: " + temperature);
+        } else if (message.equals("Qual a temperatura minima de hoje?")) {                        	   
+      	   int temperature = random.nextInt(0, 20);
+      	   return ("Temperatura minima de hoje: " + temperature);
+        } else {
+     	   return ("I don't recongnize this command!");
         }
     }
 }
